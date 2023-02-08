95306eb76b2356c3acc1ea5e550070883a8ed9b4
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-02-08 13:09:43 +0500

    added feature to only unfollow users that were followed by the bot

--- main.py
+++ main.py
@@ -19,10 +19,17 @@ def main(n=200, target_followers=600):
         api = tweepy.API(auth)
         # Get followers first so we don't accidentally unfollow a new follow.
         followers = api.get_friend_ids()
+        filename = "followings.txt"
+        if os.path.isfile(filename):
+            with open(filename, "r") as f:
+                bot_followers = [int(x.strip())
+                                 for x in f.read().split("\n") if x.strip()]
+        else:
+            bot_followers = []
 
         q = "podcast OR #PRPros OR #communications OR #prsa OR #prssa OR #prsaicon OR #publicrelations OR #comms"
 
-        followed = 0
+        followed = []
         for page in range(1, 51):
             try:
                 logging.info(f"Finding users - Page {page}")
@@ -38,10 +45,10 @@ def main(n=200, target_followers=600):
                         logging.error(e)
                         continue
                     logging.info(f"[twitter-bot] followed: {user_id}")
-                    followed += 1
-                    if followed == n:
+                    followed.append(user_id)
+                    if len(followed) == n:
                         break
-                if followed == n:
+                if len(followed) == n:
                     break
             except Exception as e:
                 logging.error(e)
@@ -56,24 +63,28 @@ def main(n=200, target_followers=600):
         unfollowed = 0
         target_followers = random.randint(
             target_followers-10, target_followers)
-        if len(followers) + followed > target_followers:
+        if len(followers) + len(followed) > target_followers:
             # Unfollow some.
-            to_unfollow = len(followers) + followed - target_followers
-            while unfollowed < to_unfollow and len(followers) > 0:
-                u = random.choice(followers)
+            to_unfollow = len(followers) + len(followed) - target_followers
+            while unfollowed < to_unfollow and len(bot_followers) > 0:
+                u = random.choice(bot_followers)
                 api.destroy_friendship(user_id=u)
                 logging.info(f"[twitter-bot] unfollowed: {u}")
-                followers.remove(u)
+                bot_followers.remove(u)
                 unfollowed += 1
+
+        with open(filename, "w") as f:
+            f.write(
+                "\n".join([str(x) for x in bot_followers] + [str(x) for x in followed]))
 
         new_followers = api.get_friend_ids()
 
         send_to_me("Twitter Bot Daily Update\n\nFollowed Today: " +
-                   str(followed) + "\nUnfollowed Today: " + str(unfollowed) + "\nTotal Followings: " + str(len(new_followers)))
+                   str(len(followed)) + "\nUnfollowed Today: " + str(unfollowed) + "\nTotal Followings: " + str(len(new_followers)))
     except Exception as e:
         send_to_me("[twitter-bot] ran into an error: " + str(e))
         logging.info(e)
 
 
 if __name__ == "__main__":
-    main(60, 250)
+    main(2, 250)
